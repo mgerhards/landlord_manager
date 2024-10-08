5ebfca1874a766310fefebf71918450a6d46a7ff
Markus <markus.gerhards@gmail.com>
2024-10-08 21:03:04 +0200

    init

---
+++ src/main/java/de/propadmin/rental_manager/RentalManagerApplication.java
@@ -0,0 +1,13 @@
+package de.propadmin.rental_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalManagerApplication.class, args);
+	}
+
+}
