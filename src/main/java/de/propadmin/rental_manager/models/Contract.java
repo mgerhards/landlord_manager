e0a8e6ce3c6047d89275a20155a2ae3fa961683c
Markus <markus.gerhards@gmail.com>
2024-10-10 10:03:31 +0200

    wip

--- src/main/java/de/propadmin/rental_manager/models/Contract.java
+++ src/main/java/de/propadmin/rental_manager/models/Contract.java
@@ -3,11 +3,10 @@ package de.propadmin.rental_manager.models;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
--- src/main/java/de/propadmin/rental_manager/models/Ticket.java
+++ src/main/java/de/propadmin/rental_manager/models/Ticket.java
@@ -2,11 +2,10 @@ package de.propadmin.rental_manager.models;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
