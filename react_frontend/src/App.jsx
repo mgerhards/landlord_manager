cd1cb70e0b2b97e182f3fdc85093afbb0c76d669
Markus <markus.gerhards@gmail.com>
2024-10-23 22:18:04 +0200

    adds logo to sidebar

--- react_frontend/src/App.jsx
+++ react_frontend/src/App.jsx
@@ -9,40 +9,32 @@ import '@adminlte-css'
 import '@bootstrap-js'
 import '@adminlte-js'
 
+import Sidebar from './components/Sidebar';
+import "./App.css"
+
 const App = () => {
+ 
+
   return (
     <div className="app-wrapper sidebar-expand-lg bg-body-tertiary sidebar-open">
       <nav className="app-header navbar navbar-expand bg-body">
-            <div className="container-fluid ">
-                <ul className="navbar-nav ms-auto">
-                    <li className="nav-item"> <a className="nav-link" data-lte-toggle="sidebar" href="#" role="button"> <i className="bi bi-list"></i> </a> </li>
-                    <li className="nav-item d-none d-md-block"> <a href="#" className="nav-link">Home</a> </li>
-                    <li className="nav-item d-none d-md-block"> <a href="#" className="nav-link">Contact</a> </li>
-                </ul>
-            </div>
-      </nav>
-      <aside className="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
-            <div className="sidebar-brand">
-              <a className="brand-link" href="/dist/pages/"> 
-                <img src="../../dist/assets/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image opacity-75 shadow"/> 
-                <span className="brand-text fw-light">AdminLTE 4</span> 
+        <div className="container-fluid ">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" data-lte-toggle="sidebar" href="#" role="button">
+                <i className="bi bi-list"></i>
               </a>
-            </div>
-     
-            <div className="sidebar-wrapper app-sidebar bg-body-secondary shadow" data-overlayscrollbars="host">
-              <nav className="mt-2"> 
-                            <ul className="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
-                                <li className="nav-item menu-open"> <a href="#" className="nav-link active"> <i className="nav-icon bi bi-speedometer"></i>
-                                        <p>
-                                            Dashboard
-                                            <i className="nav-arrow bi bi-chevron-right"></i>
-                                        </p>
-                                    </a>
-                                </li>
-                            </ul>
-              </nav>
-            </div>
-      </aside>
+            </li>
+            <li className="nav-item d-none d-md-block">
+              <a href="#" className="nav-link">Home</a>
+            </li>
+            <li className="nav-item d-none d-md-block">
+              <a href="#" className="nav-link">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Sidebar />
     </div>
   );
 };
--- react_frontend/src/components/Sidebar.jsx
+++ react_frontend/src/components/Sidebar.jsx
@@ -2,29 +2,29 @@ import React from 'react';
 
 const Sidebar = () => {
   return (
-    <aside className="main-sidebar sidebar-dark-primary ">
-      <a href="/" className="brand-link">
-        <span className="brand-text font-weight-light">AdminLTE Dashboard</span>
-      </a>
-      <div className="sidebar layout-fixed ">
-        <nav className="mt-2">
-          <ul className="nav nav-pills nav-sidebar flex-column" role="menu" data-accordion="false">
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                <i className="nav-icon fas fa-tachometer-alt"></i>
-                <p>Dashboard</p>
+    <aside className="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+            <div className="sidebar-brand">
+              <a className="brand-link" href="/dist/pages/"> 
+                <img src="/lm-logo-dark-rect.png" alt="AdminLTE Logo" className="brand-image opacity-75"/> 
+                
               </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                <i className="nav-icon fas fa-th"></i>
-                <p>Widgets</p>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </aside>
+            </div>
+     
+            <div className="sidebar-wrapper app-sidebar bg-body-secondary shadow" data-overlayscrollbars="host">
+              <nav className="mt-2"> 
+                            <ul className="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
+                                <li className="nav-item menu-open"> <a href="#" className="nav-link active"> <i className="nav-icon bi bi-speedometer"></i>
+                                        <p>
+                                            Dashboard
+                                            <i className="nav-arrow bi bi-chevron-right"></i>
+                                        </p>
+                                    </a>
+                                </li>
+                            </ul>
+              </nav>
+            </div>
+      </aside>
+   
   );
 };
 
