c5bcbf08d513cce5c6dd779122228e90a460f02c
Markus <gerhards@gendis.de>
2024-11-02 23:35:16 +0100

    fetching data works

--- react_frontend/src/components/RealEstate/Overview.jsx
+++ react_frontend/src/components/RealEstate/Overview.jsx
@@ -6,39 +6,44 @@ const RealEstateOverview = () => {
     const [realEstateObjects, setRealEstateObjects] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:8080/realEstateObjects')
+        fetch('http://localhost:8080/realEstateObjects',{
+                credentials: 'include',  // Add this line to send cookies
+                headers: {
+                    'Accept': 'application/json'
+                }
+            })
             .then(response => response.json())
             .then(data => setRealEstateObjects(data._embedded.realEstateObjects))
             .catch(error => console.error('Error fetching data:', error));
     }, []);
 
     return (
-        <div className="card">
-            <div className="card-header">
-                <h3 className="card-title">Immobilien</h3>
-            </div>
-            <div className="card-body">
-                Hier können Sie Ihre Immobilien verwalten.
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Adress</th>
-                            <th>Size</th>
-                            <th>Number of Rooms</th>
-                            <th>Description</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {realEstateObjects.map((obj) => (
-                            <tr key={obj.id}>
-                                <td>{obj.adress}</td>
-                                <td>{obj.size}</td>
-                                <td>{obj.numberOfRooms}</td>
-                                <td>{obj.description}</td>
+        <div className="container justify-content-center align-items-center vh-100">
+            <h2>Immobilien</h2>           
+            <div className="card">               
+                <div className="card-body">
+                    Hier können Sie Ihre Immobilien verwalten.
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Adress</th>
+                                <th>Size</th>
+                                <th>Number of Rooms</th>
+                                <th>Description</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {realEstateObjects.map((obj) => (
+                                <tr key={obj.id}>
+                                    <td>{obj.adress}</td>
+                                    <td>{obj.size}</td>
+                                    <td>{obj.numberOfRooms}</td>
+                                    <td>{obj.description}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
