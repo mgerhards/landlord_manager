fc2e51d5cc168d55f76b2b41ce329bb7993e4aa2
Markus <gerhards@gendis.de>
2025-07-22 23:45:53 +0200

    fix: update frontend command to run in development mode and add sourcemap to build configuration

--- react_frontend/vite.config.js
+++ react_frontend/vite.config.js
@@ -6,6 +6,9 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
